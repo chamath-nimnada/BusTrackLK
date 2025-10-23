129e1ca180482f0b0e26f89e39823252b63f4fc6
oshanda matheesha <oshandamathee02@gmail.com>
2025-10-23 13:59:17 +0530

    changes

--- mobile/driver-app/lib/main.dart
+++ mobile/driver-app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'screens/splash_screen.dart'; // Should point to splash screen
+// --- This line is correct: Import the SplashScreen ---
+import 'screens/splash_screen.dart';
 import 'utils/auth_provider.dart';
 import 'utils/language_provider.dart';
 
@@ -30,12 +31,13 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF4263EB),
         fontFamily: 'Roboto',
         textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.black, // Makes text inside fields visible
-          displayColor: Colors.black,
-        ),
+              bodyColor: Colors.black, // Makes text inside fields visible
+              displayColor: Colors.black,
+            ),
       ),
-      // This should launch the SplashScreen, which then decides the next screen
+      // --- This line is correct: Set home to SplashScreen ---
       home: const SplashScreen(),
     );
   }
 }
+
